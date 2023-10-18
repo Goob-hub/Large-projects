f1ce4927bc7d6e0cc8215a3cf8acb6dd79f00237
Goob-hub <solizg02@gmail.com>
2023-10-18 16:01:34 -0500

    Added mongoDB and mongoose from npm and started a basic database to store tasks.

--- todoapp/index.js
+++ todoapp/index.js
@@ -1,33 +1,49 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
+const url = `mongodb://127.0.0.1:27017/taskListDB`;
 const app = express();
 const port = 3000;
 
-let sortType = "all";
-let tasksArr = [
-    {name: "Eat breakfast", type: "daily"},
-    {name: "Program", type: "work"}
-];
-
+mongoose.connect(url);
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const itemSchema = new mongoose.Schema({
+    name: String,
+    type: String
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+let sortType = "all";
+
 app.post("/createTask", (req, res) => {
     let data = {name: req.body.task, type: req.body.taskType};
-    tasksArr.push(data);
-    res.render("index.ejs", {tasks: tasksArr, sort: sortType});
+    
+    res.render("index.ejs", {tasks: defaultItems, sort: sortType});
 });
 
 app.post("/sortTasks", (req, res) => {
     sortType = req.body.taskSort;
-    res.render("index.ejs", {tasks: tasksArr, sort: sortType});
+    res.render("index.ejs", {tasks: defaultItems, sort: sortType});
 });
 
-app.get("/", (req, res) => {
-    res.render("index.ejs", {tasks: tasksArr, sort: sortType});
+app.get("/", async (req, res) => {
+    let tasksArray = await getTasksFromDB();
+    res.render("index.ejs", {tasks: tasksArray, sort: sortType});
 });
 
 app.listen(port, () => {
     console.log(`Server is now running on ${port}` );
 });
+
+async function getTasksFromDB() {
+    try {
+        const tasks = await Item.find();
+        return tasks;
+    } catch (error) {
+        console.log(error)
+    }
+}
