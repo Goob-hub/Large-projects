26cfd920434442febc767e19229ab1f0c83e4b63
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2023-11-01 13:44:27 -0500

    Got passport to barley work, login works fine still but I still havent figured out how to save cookies and use them.

--- Secrets/index.js
+++ Secrets/index.js
@@ -3,8 +3,10 @@ import 'dotenv/config'
 import bcrypt from 'bcrypt'
 import bodyParser from "body-parser";
 import express from "express";
+import session from 'express-session'
 import pg from "pg";
-
+import passport from 'passport';
+import passportLocal from 'passport-local'
 
 const db = new pg.Client({
   user: process.env.DB_USER,
@@ -23,13 +25,54 @@ db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
+
+passport.use(new passportLocal.Strategy(
+    async function(username, password, done) {
+        try {
+            const text = "SELECT * FROM users WHERE username = $1";
+            const values = [username];
+            const result = await db.query(text, values);
+            const user = result.rows[0];
+            const match = await bcrypt.compare(password, user.password);
+            
+            if (!user) { return done(null, false); }
+            if (!match) { return done(null, false); }
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+
+    }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+  
+passport.deserializeUser((user, done) => {
+    console.log(user);
+    db.query('SELECT * FROM users WHERE username = $1', [user.username], (err, results) => {
+      if(err) {
+        console.log('Error when selecting user on session deserialize', err);
+        return done(err)
+      }
+  
+      done(null, results.rows[0])
+    })
+});
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
 });
 
 app.get("/login", (req, res) => {
-    res.render("login.ejs");
+    res.render("login.ejs")
 });
 
 app.get("/register", (req, res) => {
@@ -44,6 +87,10 @@ app.get("/submit", (req, res) => {
     res.render("submit.ejs");
 });
 
+app.get('/secrets', (req, res) => {
+    res.render("secrets.ejs")
+});
+
 app.post("/register", async (req, res) => {
     bcrypt.hash(req.body.password, saltRounds, async function(err, hash) {
         try {
@@ -51,6 +98,7 @@ app.post("/register", async (req, res) => {
             const text = `INSERT INTO users(username, password) VALUES($1, $2)`;
             const values = [req.body.username, hashPassword];
             const result = await db.query(text, values);
+            
             res.redirect("/");
         }  catch (error) {
             console.log("this is an error", error);
@@ -59,23 +107,28 @@ app.post("/register", async (req, res) => {
     });
 });
 
-app.post("/login", async (req, res) => {
-    try {
-        const text = "SELECT password FROM users WHERE username = $1";
-        const values = [req.body.username];
-        const result = await db.query(text, values);
-        const match = await bcrypt.compare(req.body.password, result.rows[0].password);
-
-        if(!match){
-            res.render("login.ejs", {error: "Incorrect username or password."});
-        } else {
-            res.render("secrets.ejs");
-        }
-    } catch (error) {
-        console.log("this is an error", error);
-        res.render("login.ejs", {error: error.constraint});
-    }
+app.post('/login', passport.authenticate('local', {failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect("/secrets");
 });
+
+// app.post("/login", async (req, res) => {
+//     try {
+//         const text = "SELECT password FROM users WHERE username = $1";
+//         const values = [req.body.username];
+//         const result = await db.query(text, values);
+//         const match = await bcrypt.compare(req.body.password, result.rows[0].password);
+
+//         if(!match){
+//             res.render("login.ejs", {error: "Incorrect username or password."});
+//         } else {
+//             res.render("secrets.ejs");
+//         }
+//     } catch (error) {
+//         console.log("this is an error", error);
+//         res.render("login.ejs", {error: error.constraint});
+//     }
+// });
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
