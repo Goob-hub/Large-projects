0a74d2e953f756d61d8e9403e6e672451874b58c
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-11-20 10:20:33 -0600

    changed file name and reformatted some html
    home.js -> sort_reviews.js
    
    reformatted html in book_review.ejs and hom.ejs

--- book_reviews/public/js/sort_reviews.js
+++ book_reviews/public/js/sort_reviews.js
@@ -1,7 +1,10 @@
+//ONLY USE ON WEB PAGES THAT DISPLAY BOOK REVIEWS, MAKE SURE IT'S AFTER THE NAV-BAR SCRIPT
+
 function sortReviews() {
     //Code here will handle sorting the reviews on the current page
+    
 }
 
 window.addEventListener("sortreviews", e => {
-    sortReviews()
+    sortReviews();
 });
