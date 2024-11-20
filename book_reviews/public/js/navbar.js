e91f7d5066570eb7df60f66d8b66bcfec831ebc3
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-11-20 12:49:12 -0600

    sort book reviews feature complete
    --MAIN CHANGES--
    
    sort_reviews.js can now be added to any page that displays book reviews. This script stores the users selected sorting method in localstorage so that it persists
    
    sort_reviews.js sorts book reviews by manipulating the html of the book-review-container element where all book reviews are displayed.
    
    --OTHER CHANGES--
    
    Removed sort-review functionality from navbar.js since that script is supposed to only contain code pertaining to the nav-bar.
    
    changed and added a few html elements in create_review.ejs, book_review.ejs, and navbar.ejs

--- book_reviews/public/js/navbar.js
+++ book_reviews/public/js/navbar.js
@@ -1,19 +1,2 @@
-const sortMethodSelection = document.querySelector("#sort-menu");
-const sortReviews = new CustomEvent('sortreviews');
 
-if(!localStorage.getItem("sort_method")) {
-    localStorage.setItem("sort_method", "highest_rated");
-}
-
-sortMethodSelection.addEventListener("change", e => {
-    let sortMenu = e.target;
-    
-    for(const option of sortMenu.children) {
-        if(option.selected) {
-            localStorage.setItem("sort_method", option.value);
-        }
-    }
-
-    window.dispatchEvent(sortReviews);
-});
 
--- book_reviews/public/js/sort_reviews.js
+++ book_reviews/public/js/sort_reviews.js
@@ -1,10 +1,84 @@
-//ONLY USE ON WEB PAGES THAT DISPLAY BOOK REVIEWS, MAKE SURE IT'S AFTER THE NAV-BAR SCRIPT
+const sortMethodSelection = document.querySelector("#sort-menu");
 
-function sortReviews() {
-    //Code here will handle sorting the reviews on the current page
-    
+let bookReviewContainer = document.querySelector(".book-review-container");
+let sortMenu = document.querySelector("#sort-menu");
+
+if(!localStorage.getItem("sort_method")) {
+    localStorage.setItem("sort_method", "highest_rated");
 }
 
-window.addEventListener("sortreviews", e => {
-    sortReviews();
+function sortBookReviews() {
+    let sortingMethod = localStorage.getItem("sort_method");
+    let reviews = getReviews();
+    
+    //Sorts book reviews based on user selected sort method
+    switch (sortingMethod) {
+        case "highest_rated":
+            reviews = reviews.sort((a, b) => {
+                return b.rating - a.rating;
+            });
+            changeReviewHtml(reviews);
+            break;
+        case "lowest_rated":
+            reviews = reviews.sort((a, b) => {
+                return a.rating - b.rating;
+            });
+            changeReviewHtml(reviews);
+            break;
+        default:
+            console.error("Could not sort book reviews on this page");
+            break;
+    }
+}
+
+function changeReviewHtml(sortedReviewsArray) {
+    if(!sortedReviewsArray) {
+        console.error("Did not recieve sorted html in the changeReviewHtml function in sort_reviews.js!");
+        return;
+    }
+
+    //Emptys current html for sorted html to be added
+    bookReviewContainer.innerHTML = "";
+
+    //Makes sure correct dropdown menu value is selected for user feedback
+    sortMenu.childNodes.forEach(child => {
+        if(child.value === sortingMethod) {
+            child.selected = true;
+        } else {
+            child.selected = false;
+        }
+    });
+
+    //Injects html of each review in order based on selected sort method
+    reviews.forEach(review => {
+        bookReviewContainer.innerHTML += review.html
+    });
+}
+                
+function getReviews() {
+    let reviews = [];
+    let bookReviews = document.querySelectorAll(".book-review");
+    
+    bookReviews.forEach(bookReview => {
+        reviews.push({
+            html: bookReview.outerHTML,
+            rating: parseInt(bookReview.querySelector("#rating").textContent)
+        });
+    });
+
+    return reviews;
+}
+                
+sortMethodSelection.addEventListener("change", e => {
+    let sortMenu = e.target;
+    
+    for(const option of sortMenu.children) {
+        if(option.selected) {
+            localStorage.setItem("sort_method", option.value);
+        }
+    }
+
+    sortBookReviews();
 });
+
+sortBookReviews();
