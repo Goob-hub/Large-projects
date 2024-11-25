11ee0fd78341f5776110bfd6c1c6f98093e4b14e
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-11-25 13:27:27 -0600

    Animated nav menu
    --CHANGELOG--
    
    - Animated nav-menu with css and javascript, styling still incomplete

--- book_reviews/public/js/navbar.js
+++ book_reviews/public/js/navbar.js
@@ -1,6 +1,24 @@
 const navBtn = document.querySelector("#nav-btn");
+let navMenu = document.querySelector("#nav-menu");
 
 navBtn.addEventListener("click", e => {
+    let openTimeout = 300;
+    let closeTimeout = 50;
+
     navBtn.classList.toggle("open");
+    navMenu.classList.toggle("open");
+    navMenu.classList.toggle("hidden");
+
+    if(navBtn.classList.contains("open")) {
+        for (const htmlEl of navMenu.children) {
+            setTimeout((timeout) => {
+                htmlEl.classList.toggle("off");
+            }, openTimeout += 100);
+        }
+    } else {
+        for (const htmlEl of navMenu.children) {
+            htmlEl.classList.toggle("off");
+        }
+    }
 });
 
